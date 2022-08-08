47fc339ae5f815fbd9b1ee214dad84ce173b77d6
bryaalbani <79945521+bryaalbani@users.noreply.github.com>
2022-08-08 12:15:21 -0500

    Add files via upload

---
+++ Reto/IAC/main.tf
@@ -0,0 +1,56 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 2.26"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "StorageAccount-ResourceGroup"
+    storage_account_name = "tfstateprodprueba"
+    container_name       = "tfstate"
+    key                  = "prod.terraform.tfstate"
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "testbp1"
+  location = "East US 2"
+}
+
+resource "azurerm_container_registry" "cr" {
+  name                = "PruebaCr"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Premium"
+}
+
+resource "azurerm_kubernetes_cluster" "kc" {
+  name                = "Prueba-aks1"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "TestBpaks1"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 2
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Production"
+  }
+}
+
